f5ddb8d78450ab67c4477f18014a0c4a0243f83c
HaZ3R0 <megamackan@outlook.com>
2024-03-01 12:16:28 +0100

    Add project files.

---
+++ Uppgift 6.8 metoder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace fortnite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal för att undersöka det");
+            int tal = int.Parse(Console.ReadLine());
+            Primtal(tal);
+        }
+
+        static void Primtal(int tal)
+        {
+            int deltal = 0;
+            int primtal = 0;
+
+            for (int i = 1; i < tal; i++)
+            {
+
+                for (int j = 2; j < tal; j++)
+                {
+                    if (i % j == 0 && i != j)
+                    {
+                        deltal++;
+                    }
+                }
+                if (deltal == 0)
+                {
+                    primtal++;
+                }
+                deltal = 0;
+            }
+            Console.WriteLine(primtal);
+        }
+    }
+}
